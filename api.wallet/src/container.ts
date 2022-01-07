f9a6878a47acb4cd6162466c0b959007495afb9b
sanpedro ezequiel <sanpedrofabianezequiel@gmail.com>
2022-01-07 12:11:32 -0300

    Change inyection dependency to MSSQL

--- api.wallet/src/container.ts
+++ api.wallet/src/container.ts
@@ -7,6 +7,9 @@ import { SubscriptionService } from './services/subscription.service';
 import { MovementMySQLRepository } from './services/repositories/impl/mysql/movement.repository';
 import { BalanceMySQLRepository } from './services/repositories/impl/mysql/balance.repository';
 import { MovementService } from './services/movemnt.service';
+import { SubscriptionMSSQLRepository } from './services/repositories/impl/mssql/subscription.repository';
+import { BalanceMSSQLRepository } from './services/repositories/impl/mssql/balance.repository';
+import { MovementMSSQLRepository } from './services/repositories/impl/mssql/movement.repository';
 
 export default (app: express.Application)=>{
     const container =  createContainer({
@@ -15,9 +18,16 @@ export default (app: express.Application)=>{
 
     container.register({
         //repositories
-        subscriptionRepository : asClass(SubscriptionMySQLRepository).scoped(),
+        /*subscriptionRepository : asClass(SubscriptionMySQLRepository).scoped(),
         movementRepository:asClass(MovementMySQLRepository).scoped(),
         balanceRepository:asClass(BalanceMySQLRepository).scoped(),
+        */
+        
+
+        subscriptionRepository : asClass(SubscriptionMSSQLRepository).scoped(),
+        movementRepository:asClass(MovementMSSQLRepository).scoped(),
+        balanceRepository:asClass(BalanceMSSQLRepository).scoped(),
+
 
         //Services //Mapiando depencias
         subscriptionService:asClass(SubscriptionService).scoped(),
--- api.wallet/src/services/repositories/ISubscription.repository.ts
+++ api.wallet/src/services/repositories/ISubscription.repository.ts
@@ -1,10 +1,10 @@
 import { Subscription } from "./model/subscription";
 
-export interface SubscriptionRepository{
+export interface ISubscriptionRepository{
     all(): Promise<Subscription[]>;
     find(id:number) :Promise<Subscription | null>;
     store(entry: Subscription) :Promise<void>;
     update(entry:Subscription):Promise<void>;
     remove(id:Number):Promise<void>;
-    findByUserAndCode(user_id: number,code:string):Promise<Subscription | null >
+    findByUserIdAndCode(user_id: number,code:string):Promise<Subscription | null >
 }
