175bc5e1ddcce83eedcdf2b952f0bf357ea1f997
sanpedro ezequiel <sanpedrofabianezequiel@gmail.com>
2022-01-07 01:13:25 -0300

    Create Connecton SQL Server

--- api.wallet/src/container.ts
+++ api.wallet/src/container.ts
@@ -4,6 +4,9 @@ import { TestService } from './services/test.service';
 import { scopePerRequest } from 'awilix-express';
 import { SubscriptionMySQLRepository } from './services/repositories/impl/mysql/subscription.respository';
 import { SubscriptionService } from './services/subscription.service';
+import { MovementMySQLRepository } from './services/repositories/impl/mysql/movement.repository';
+import { BalanceMySQLRepository } from './services/repositories/impl/mysql/balance.repository';
+import { MovementService } from './services/movemnt.service';
 
 export default (app: express.Application)=>{
     const container =  createContainer({
@@ -13,10 +16,12 @@ export default (app: express.Application)=>{
     container.register({
         //repositories
         subscriptionRepository : asClass(SubscriptionMySQLRepository).scoped(),
-
+        movementRepository:asClass(MovementMySQLRepository).scoped(),
+        balanceRepository:asClass(BalanceMySQLRepository).scoped(),
 
         //Services //Mapiando depencias
         subscriptionService:asClass(SubscriptionService).scoped(),
+        movementService:asClass(MovementService).scoped(),
         testService : asClass(TestService).scoped()
     });
 
